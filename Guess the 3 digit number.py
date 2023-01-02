695926889a014438b2aee718be8cdc72e6b73534
Muzaffer Deliali <deliali_muzaffer@hotmail.com>
2023-01-02 16:09:55 +0300

    Update Guess the 3 digit number.py

--- Guess the 3 digit number.py
+++ Guess the 3 digit number.py
@@ -2,8 +2,8 @@ import random
 
 
 def all_unique(x):
-   seen = set()
-   return not any(a in seen or seen.add(a) for a in x)
+    seen = set()
+    return not any(a in seen or seen.add(a) for a in x)
 
 
 numbers = random.sample(range(1, 10), 3)
@@ -13,31 +13,29 @@ else:
     numbers = random.sample(range(1, 10), 3)
 
 lst = list(map(int, input(
-        "Enter the integer elements of list: ")))[:3]
+    "Enter the integer elements of list: ")))[:3]
 
-if all_unique(lst):
-    print('The list is:', lst)   # printing the list
-    if lst == numbers:
-        print("that's correct")
 
-    def common_member(numbers, lst):
-        set_numbers = set(numbers)
-        set_lst = set(lst)
+def common_member(numbers, lst):
+    set_numbers = set(numbers)
+    set_lst = set(lst)
 
-        if len(set_numbers.intersection(set_lst)) > 0:
-            return set_numbers.intersection(set_lst)
-        else:
-            print("no common elements")
+    if len(set_numbers.intersection(set_lst)) > 0:
+        return set_numbers.intersection(set_lst)
+    else:
+        print("0 point")
 
+
+while all_unique(lst):
+    print('The list is:', lst)  # printing the list
 
     while lst != numbers:
-        print(len(common_member(numbers, lst)), "number(s) in common")
         lst = list(map(int, input(
             "I guess it's: ")))[:3]
-        if all_unique(lst):
-            print('The list is:', lst)
+        if lst == all_unique(lst):
+            print('The unique list is:', lst)
+            print(len(common_member(numbers, lst)), "number(s) in common", "That's close, keep trying")
             if lst == numbers:
                 print("that's correct")
-
-else:
-    print('pick unique numbers')
+        else:
+            print('pick unique number')
